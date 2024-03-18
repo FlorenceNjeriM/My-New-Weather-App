dcedf52530b8e293103b9243fd77af6b2de2cb68
FlorenceNjeriM <159240271+FlorenceNjeriM@users.noreply.github.com>
2024-03-18 19:05:24 +0300

    added axios

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ function handleSearchSubmit(event) {
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
 }
+let apiKey= "2c694e6242tb2c1do6178043fab30107";
+let apiUrl =
+`https://api.shecodes.io/weather/v1/current?query=${city}&key=${2c694e6242tb2c1do6178043fab30107}`;
+
 let formElement = document.querySelector("#search-form");
 formElement.addEventListener("submit", handleSearchSubmit);
