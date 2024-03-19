52491755c4c1aa7e6ab8e6c7bf2f99840694682b
FlorenceNjeriM <159240271+FlorenceNjeriM@users.noreply.github.com>
2024-03-19 14:09:49 +0300

    Added live weather icon

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ function updateWeather(response) {
   let windElement = document.querySelector("#wind");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#temperature-icon");
 
   console.log(response.data);
 
@@ -16,6 +17,7 @@ function updateWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windElement.innerHTML = `${response.data.wind.speed}km/h`;
   timeElement.innerHTML = formatDate(date);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="temperature-icon"/>`;
 }
 function formatDate(date) {
   let hours = date.getHours();
